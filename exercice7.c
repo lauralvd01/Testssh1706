49c33e27151ac67e6c9a771c592e05e22e42ef9f
lauralvd01 <laura.levraud1@etu.univ-lorraine.fr>
2022-06-20 11:37:09 +0200

    7.1

---
+++ exercice7.c
@@ -0,0 +1,12 @@
+#include "exercice7.h"
+#include<stdio.h>
+
+void print_tableau(int tab[],len){
+    for(int i = 1;i<=len;++i){
+        printf("|%d|",tab[i]);
+    }
+    return
+}
+
+// Stocke dans un tableau les nombres pairs plus petits que 100, et affiche ce tableau
+int main() {}
