79cc7a502214168bbaff0b77b9ca1a6a95b4fb33
lauralvd01 <laura.levraud1@etu.univ-lorraine.fr>
2022-06-21 09:20:36 +0200

    Cours.2106.1

--- Cours.c
+++ Cours.c
@@ -1,1 +1,15 @@
-int main() {}
+#include<stdio.h>
+#include <string.h>
+
+int main() {
+    int tab[] = {5,12,8,1};
+    char st1[] = {'h','e','l','l','o','\0'};
+    char st2[] = "hello";
+    size_t lenst1 = strlen(st1);
+    size_t  lenst2 = strlen(st2);
+
+    printf("st1 =  %s\n",st1);
+    printf("st2 = %s\n",st2);
+    printf("len st1 = %ld\n",lenst1);
+    printf("len st2 = %ld\n",lenst2);
+}
